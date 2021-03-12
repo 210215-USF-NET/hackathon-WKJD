d0564e18492cb25e7bccf0ebc93bd971772a74f9
KMurphy-5 <kevincitorios@gmail.com>
2021-03-12 07:58:12 -0800

    fix of user models

--- VCVeterans/VCVModels/User.cs
+++ VCVeterans/VCVModels/User.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VCVModels
 {
-    class Class2
+    class User
     {
+        [Key]
+        public int UserID { get; set; }
+
+        public string Email { get; set; }
     }
 }
